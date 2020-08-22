42da69daae9378a5d83b6bcbd8bedb877755d223
Vaskar <vaskar.nath@mail.utoronto.ca>
2020-08-22 15:39:38 -0600

    added map

---
+++ backend/heat_map.py
@@ -0,0 +1,20 @@
+from urllib.request import urlopen
+import json
+import pandas as pd
+import plotly
+import plotly.express as px
+import matplotlib.pyplot as plt
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
+
+df = pd.read_csv("https://raw.githubusercontent.com/plotly/datasets/master/fips-unemp-16.csv",
+                   dtype={"fips": str})
+
+fig = px.choropleth(df, geojson=counties, locations='fips', color='unemp',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           scope="usa",
+                           labels={'unemp':'unemployment rate'}
+                          )
+fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+plotly.offline.plot(fig, filename='map.png')
