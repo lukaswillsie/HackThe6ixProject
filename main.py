150743be0c9e9b969e41097577913e329c69ce45
Vaskar <vaskar.nath@mail.utoronto.ca>
2020-08-23 05:37:46 -0600

    m

--- main.py
+++ main.py
@@ -45,6 +45,8 @@ def background_process():
     try:
         location = request.args.get('location', 0, type=str).split(" ")
         lst = obj.getKeyMetrics(location[0], location[1])
+        map_loc = obj.get_graphs(location[0], location[1])
+        print(map_loc)
         for i in range(len(lst)):
             if lst[i] < 0:
                 lst[i] = 0
