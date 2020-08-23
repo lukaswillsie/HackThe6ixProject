c93332711c19318fc0c0c08697fa927d541cb568
Lukas Willsie <lukaswillsie@gmail.com>
2020-08-23 07:43:42 -0300

    Autocomplete JS

--- static/auto-complete.js
+++ static/auto-complete.js
@@ -1,6 +1,14 @@
-function initialize() {
-    var input = document.getElementById('searchTextField');
-    new google.maps.places.Autocomplete(input);
-  }
+function initAutoComplete() {
+  var input = document.getElementById('searchTextField');
+  autocomplete = new google.maps.places.Autocomplete(input, {types : ["geocode"]});
+  autocomplete.setFields(["address_component"]);
+  autocomplete.addListener("place_changed", getDataFromSelection);
+  console.log("Got here")
+}
+
+function getDataFromSelection() {
+  const place = autocomplete.getPlace();
+  console.log(place.address_components);
+}
   
-  google.maps.event.addDomListener(window, 'load', initialize);
+  // google.maps.event.addDomListener(window, 'load', initialize);
